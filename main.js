604370c4dc29271e1f016cbf1b36e44ec93abe8b
lucas rouchy <lucasrouchy1@gmail.com>
2019-11-20 15:44:05 -0800

    Fixed the ride count and added the graph

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
 alert("Hey Guy")
 
-$(updateview)
+const BASE_URL = "https://zagster-service.herokuapp.com"
 
-function updateview(){
-    alert("Using jQuery!")
-}
+ $(updateGraph)
+ $ (updateView)
+
+ function updateView() {
+   alert('OHMAN')
+   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+ }
+
+ function updateRideCount(data) {
+   numberOfRides = data.count
+   $("h2#rideCount").html(numberOfRides)
+ }
+
+
+ 
+ 
+ 
+ function updateGraph() {
+ var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+ }
